36620021d0b8469ee155e9076b7ac7067dddb7f1
priya <pjha18220@gmail.com>
2024-01-28 22:50:30 +0545

    todolist.js modified

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,15 +1,36 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 
 function Todolist() {
-  const [activity, setActivity]= useState("");
+  const [activity, setActivity] = useState("");
+  const [listData, setlistData] = useState([]);
+
+  function addActivity() {
+    setlistData(prevListData => {
+      const updatedList = [...prevListData, activity];
+      console.log(updatedList);
+      setActivity('');
+      return updatedList;
+    });
+  }
+
   return (
     <>
-    <div  className='peak'>
-      <div className='header'>Todolist</div>
-      <input type='text' placeholder='Add Activity'value={activity} onChange={(e)=>setActivity(e.target.value)}/>
-    </div>
-     </>
-  )
+      <div className='peak'>
+        <div className='header'>Todolist</div>
+        <input
+          type='text'
+          placeholder='Add Activity'
+          value={activity}
+          onChange={(e) => setActivity(e.target.value)}
+        />
+        <button onClick={addActivity}>add</button>
+        <p className='list-heading'>Here is your List :{")"}</p>
+      </div>
+    </>
+  );
 }
 
-export default Todolist
+export default Todolist;
+
+
+
